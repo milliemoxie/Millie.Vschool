186df711b5641c0d73ae2e8da1c44978ba7f35cb
milliemoxie <60668384+milliemoxie@users.noreply.github.com>
2020-02-10 13:55:10 -0700

    Add files via upload

---
+++ MOD 0/Grocery Store JS/script.js
@@ -0,0 +1,18 @@
+var shopper = {
+    name: "Jacob",
+    age: 27,
+    needsFood: true,
+    groceryCart: "empty",
+    groceryList = [
+        "apples",
+        "milk",
+        "bread",
+        "cereal",
+        "juice",
+        "eggs",
+        "cheese",
+    ],
+    if (groceryCart = "empty") {console.log = "Jacob still needs groceries!"},
+    else (needsFood = false) {console.log = "We're done."}
+
+}
